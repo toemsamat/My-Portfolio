d6d6ed73ea95cfbeab3c68645109a797c5cc7222
toemsamat <samattoem212@gmail.com>
2025-05-24 22:31:09 +0700

    Fix: Add STATIC_ROOT for collectstatic

--- config/settings.py
+++ config/settings.py
@@ -15,6 +15,15 @@ import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 import environ
 import os
 
