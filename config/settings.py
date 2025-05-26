e2048b449a69757d547e0237b6fa74702f8f4a70
toemsamat <samattoem212@gmail.com>
2025-05-26 21:19:24 +0700

    change resume

--- config/settings.py
+++ config/settings.py
@@ -77,8 +77,11 @@ if os.environ.get('RENDER') == 'true':
     }
 else:
     DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///db.sqlite3', conn_max_age=600)
-}
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
