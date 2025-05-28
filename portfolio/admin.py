623293f46475727bb142810a33980324d7de5695
toemsamat <samattoem212@gmail.com>
2025-05-28 13:47:33 +0700

    add certificate admin.py

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Profile, Skill, Experience, Project, BlogPost, ContactMessage
+from .models import Certificate, Profile, Skill, Experience, Project, BlogPost, ContactMessage
 
 admin.site.register(Profile)
 admin.site.register(Skill)
@@ -7,5 +7,6 @@ admin.site.register(Experience)
 admin.site.register(Project)
 admin.site.register(BlogPost)
 admin.site.register(ContactMessage)
+admin.site.register(Certificate)
 
 # Register your models here.
